f9ad7999ad41a0e3da7d0291ed746bcde90415b7
Mihau <michal.pryba.mail@gmail.com>
2023-10-24 23:20:00 +0200

    rewrite whole code

--- main.go
+++ main.go
@@ -1,48 +1,11 @@
 package main
 
-<<<<<<< Updated upstream
-import "math/rand"
-
-var seed int64 = 4123456275344
-var r *rand.Rand = rand.New(rand.NewSource(seed))
-=======
 var chunk_size int = 1024
->>>>>>> Stashed changes
 
 type Vector2 struct {
 	x, y float64
 }
 
-<<<<<<< Updated upstream
-type pair struct {
-	x, y int
-}
-
-type chunk struct {
-	x, y                   int
-	vec1, vec2, vec3, vec4 Vector2
-	perlin                 [][]float64
-}
-
-func main() {
-	chunks := make(map[pair]chunk)
-
-	newChunk := chunk{
-		x:    0,
-		y:    0,
-		vec1: randomRotatedVector2(),
-		vec2: randomRotatedVector2(),
-		vec3: randomRotatedVector2(),
-		vec4: randomRotatedVector2(),
-	}
-
-	perlin(&newChunk)
-
-	chunks[pair{0, 0}] = newChunk
-
-	create_image(newChunk.perlin, "new")
-=======
 func main() {
 	create_image(mapGen(21221, 23), "img")
->>>>>>> Stashed changes
 }
--- perlin_gen.go
+++ perlin_gen.go
@@ -4,33 +4,6 @@ import (
 	"math"
 )
 
-<<<<<<< Updated upstream
-// My implementation of perlin noise generator
-var chunk_size int = 1024
-
-// calculate dot product of influeance vector and offset vector
-func dot(g Vector2, x, y int) float64 {
-	var vector_length float64 = math.Sqrt(float64(math.Pow(float64(x+1), 2) + math.Pow(float64(y+1), 2)))
-	return g.x*(float64(x)+1)/vector_length + g.y*(float64(y)+1)/vector_length
-}
-
-// create influence vector
-func randomRotatedVector2() Vector2 {
-	var vector Vector2
-	vector.x, vector.y = math.Sincos(r.Float64() * math.Pi * 2)
-	return vector
-}
-
-// bicubic interpolation
-func interpolate(a0 float64, a1 float64, w float64) float64 {
-	return ((a1-a0)*(3.0-w*2.0)*w*w + a0)
-}
-
-// func randomGradient(seed_val int) [][]float64 {
-// 	// create 2 dimensional slice that will store dot products of random influeance vector and offset vectors
-// 	var chunk [][]float64
-// 	var vector Vector2 = randomRotatedVector2(seed_val) //create influeance vector by given seed
-=======
 func dot(ix, iy int, x, y float64) float64 {
 	vec := randomVec2(ix, iy)
 	dx := x - float64(ix)
@@ -81,72 +54,17 @@ func perlin(x, y float64) float64 {
 	return interpolate(ix0, ix1, sy)
 }
 
-// func randomGradient() [][]float64 {
-// 	// create 2 dimensional slice that will store dot products of random influeance vector and offset vectors
-// 	var chunk [][]float64
-// 	var vector Vector2 = randomRotatedVector2(seed) //create influeance vector by given seed
->>>>>>> Stashed changes
-
-// 	for i := 0; i < chunk_size; i++ {
-// 		var row []float64
-// 		for j := 0; j < chunk_size; j++ {
-<<<<<<< Updated upstream
-// 			row = append(row, dot(vector, j, i))
-=======
-// 			row = append(row, dot(vector, float64(j), float64(i)))
->>>>>>> Stashed changes
-// 		}
-// 		chunk = append(chunk, row)
-// 	}
-// 	return chunk
-// }
-
-<<<<<<< Updated upstream
-// generate chunk of perlin noise
-func perlin(ch *chunk) {
-=======
 func mapGen(x, y float64) [][]float64 {
 	var chunk [][]float64
->>>>>>> Stashed changes
 
 	for i := 0; i < chunk_size; i++ {
 		var row []float64
 		for j := 0; j < chunk_size; j++ {
-<<<<<<< Updated upstream
-
-			sx := float64(j) / float64(chunk_size)
-			sy := float64(i) / float64(chunk_size)
-
-			n0 := dot(ch.vec1, j, i)
-			n1 := dot(ch.vec2, j, i)
-			ix0 := interpolate(n0, n1, sx)
-
-			n0 = dot(ch.vec3, j, i)
-			n1 = dot(ch.vec4, j, i)
-			ix1 := interpolate(n0, n1, sx)
-
-			value := interpolate(ix0, ix1, sy)
-
-			row = append(row, value)
-=======
 			row = append(row, (perlin(float64(j)/float64(chunk_size)+x, float64(i)/float64(chunk_size))+1)/2+y)
->>>>>>> Stashed changes
 		}
-		ch.perlin = append(ch.perlin, row)
+		chunk = append(chunk, row)
 
 	}
-<<<<<<< Updated upstream
-=======
 
 	return chunk
->>>>>>> Stashed changes
 }
-
-// 	for i := 0; i < len(chunk); i++ {
-// 		for j := 0; j < len(chunk[i]); j++ {
-// 			fmt.Printf("%f\t", chunk[i][j])
-// 		}
-// 		fmt.Println() // Move to the next row
-// 	}
-// 	return chunk
-// }
